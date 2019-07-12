2f6aabcd127a9471819ab36d64777f4b34bd3436
Deathr0ck <Deathr0ck@gmail.com>
2019-07-12 13:52:05 +0700

    Create ListVideo Success

---
+++ lib/models/non_video_model.dart
@@ -0,0 +1,9 @@
+class NonVideoModel {
+  // Field
+
+  String name, detail, pathImage, pathVideo;
+
+  // Constructor
+
+  NonVideoModel(this.name, this.detail, this.pathImage, this.pathVideo);
+}
--- lib/screens/show_list_video.dart
+++ lib/screens/show_list_video.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
+import 'package:a_tpa/models/non_video_model.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ShowListVideo extends StatefulWidget {
   @override
@@ -6,8 +10,105 @@ class ShowListVideo extends StatefulWidget {
 }
 
 class _ShowListVideoState extends State<ShowListVideo> {
+// Explicit
+
+  Firestore firestore = Firestore.instance;
+  StreamSubscription<QuerySnapshot> subscription;
+  List<DocumentSnapshot> snapshots;
+  List<NonVideoModel> nonVideoModels = [];
+
+// Method
+
+  @override
+  void initState() {
+    super.initState();
+    readFirestore();
+  }
+
+  Future<void> readFirestore() async {
+    CollectionReference collectionReference = firestore.collection('NonVideo');
+    subscription = await collectionReference.snapshots().listen((response) {
+      snapshots = response.documents;
+      for (var snapshot in snapshots) {
+        String nameVideo = snapshot.data['Name'];
+        String detailVideo = snapshot.data['Detail'];
+        String pathImage = snapshot.data['PathImage'];
+        String pathVideo = snapshot.data['PathVideo'];
+
+        NonVideoModel nonVideoModel =
+            NonVideoModel(nameVideo, detailVideo, pathImage, pathVideo);
+        setState(() {
+          nonVideoModels.add(nonVideoModel);
+        });
+
+        // print('$pathVideo');
+
+      }
+    });
+  }
+
+  Widget showImage(int index) {
+    return Container(
+      margin: EdgeInsets.all(5.0),
+      width: 180.0,
+      height: 120.0,
+      child: Image.network(
+        nonVideoModels[index].pathImage,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  Widget showText(int index) {
+    return Container(
+      margin: EdgeInsets.all(5.0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topLeft,
+            child: Text(
+              nonVideoModels[index].name,
+              style: TextStyle(fontSize: 24.0, color: Colors.orange[900]),
+            ),
+          ),
+          Container(
+            alignment: Alignment.topLeft,
+            child: Text(nonVideoModels[index].detail),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget myDevider() {
+    return Divider(
+      height: 5.0,
+      color: Colors.grey[600],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Show List Video');
+    return Container(
+      alignment: Alignment.topLeft,
+      child: ListView.builder(
+        itemCount: nonVideoModels.length,
+        itemBuilder: (context, int index) {
+          return Container(
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    showImage(index),
+                    showText(index),
+                  ],
+                ),
+                myDevider(),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
